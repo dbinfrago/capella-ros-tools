9afadcbb297945bf3d764199c7db3b01261303e0
huyenngn <ngan-huyen.thi-nguyen@deutschebahn.com>
2023-12-21 07:42:43 +0100

    feat: Convert capella to messages

--- capella_ros_tools/modules/messages/serializer.py
+++ capella_ros_tools/modules/messages/serializer.py
@@ -18,13 +18,35 @@ class MessageDef(BaseMessageDef):
 
     def to_msg_string(self) -> str:
         """Convert message to string."""
-        msg_string = "\n".join(self.annotations) + "\n"
-        msg_string += (
-            "\n".join([f"{f.type} {f.name}" for f in self.fields]) + "\n\n"
-        )
-        msg_string += (
-            "\n\n".join([f"{e.name}\n{e.values}" for e in self.enums]) + "\n\n"
-        )
+        msg_string = "\n".join(self.annotations) + "\n\n"
+        for enum in self.enums:
+            msg_string += (
+                f"# name: {enum.name}" + "\n".join(enum.annotations) + "\n"
+            )
+            msg_string += (
+                "\n".join(
+                    [
+                        f"{value.type.name} {value.name} = {value.value}"
+                        + "\n".join(self.annotations)
+                        for value in enum.values
+                    ]
+                )
+                + "\n\n"
+            )
+
+        for field in self.fields:
+            msg_string += (
+                f"{(field.type.pkg_name + '/') if field.type.pkg_name else ''}"
+            )
+            msg_string += f"{field.type.name}"
+            if field.type.array_size == float("inf"):
+                msg_string += "[]"
+            elif field.type.array_size:
+                msg_string += f"[{field.type.array_size}]"
+            msg_string += (
+                f" {field.name}" + "\n".join(self.annotations) + "\n\n"
+            )
+
         return msg_string
 
 
@@ -36,3 +58,11 @@ class MessagePkgDef(BaseMessagePkgDef):
         msg_pkg_dir.mkdir(parents=True, exist_ok=True)
         for msg in self.messages:
             msg.to_msg_file(msg_pkg_dir / f"{msg.name}.msg")
+        for pkg in self.packages:
+            pkg.to_msg_folder(msg_pkg_dir / pkg.name)
+
+    def to_pkg_folder(self, pkg_dir: Path):
+        """Write packages to folder."""
+        pkg_dir.mkdir(parents=True, exist_ok=True)
+        for pkg in self.packages:
+            pkg.to_msg_folder(pkg_dir / pkg.name / "msg")
